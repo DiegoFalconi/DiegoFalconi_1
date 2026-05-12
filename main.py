5ac633675a376946d6ecd3e5ecea902929252a7f
DiegoFalconi <jdfalconi10@gmail.com>
2026-05-11 19:38:35 -0500

    Add simple greeting program in main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+# Programa simple de ejemplo
+
+def saludar(nombre):
+    return f"Hola, {nombre}! Bienvenido a GitHub 🚀"
+
+
+if __name__ == "__main__":
+    usuario = input("Ingresa tu nombre: ")
+    print(saludar(usuario))
